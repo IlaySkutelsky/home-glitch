b2e3086a73c29ff06320fbe6b8cbc1c40f5e9171
IlaySkutelsky <40234996+IlaySkutelsky@users.noreply.github.com>
2020-11-01 19:40:03 +0200

    modifs

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 
-uniforms1 = {
-  "time": { value: 1.0 }
-};
+let canvasTex = new THREE.Texture(generateTexture())
+canvasTex.needsUpdate = true
 
+uniforms1 = {
+  "time": { value: 1.0 },
+  "texture1": { type: "t", value: canvasTex }
+};
 
 const myMaterial = new THREE.ShaderMaterial( {
   uniforms: uniforms1,
@@ -47,10 +50,41 @@ function animate() {
 	let frameNum = requestAnimationFrame( animate );
   renderer.render( scene, camera );
 
-  uniforms1[ "time" ].value += 0.1
+  uniforms1[ "time" ].value += 0.0725
 
-  houseGroup.rotation.x = (Math.sin(frameNum*0.005)+0.5) * 0.3;
+  houseGroup.rotation.x = (Math.sin(frameNum*0.005)+0.725) * 0.4;
   houseGroup.rotation.y -= (Math.sin(frameNum*0.02)+3)*0.002;
+
+}
+
+function generateTexture() {
+
+  const canvas = document.createElement( 'canvas' );
+  canvas.width = 256;
+  canvas.height = 256;
+
+  const context = canvas.getContext( '2d' );
+  const image = context.getImageData( 0, 0, 256, 256 );
+
+  let x = 0, y = 0;
+
+  for ( let i = 0, j = 0, l = image.data.length; i < l; i += 4, j ++ ) {
+
+    x = j % 256;
+    y = ( x === 0 ) ? y + 1 : y;
+
+    let val =  Math.floor( x ^ y )
+
+    image.data[ i ] = val;
+    image.data[ i + 1 ] = val;
+    image.data[ i + 2 ] = val;
+    image.data[ i + 3 ] =  255;
+
+  }
+
+  context.putImageData( image, 0, 0 );
+
+  return canvas;
 
 }
 
